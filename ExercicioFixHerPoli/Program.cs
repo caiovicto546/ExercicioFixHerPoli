57a64ecc554e7b55bcc7a4949f671a1b8642f48b
caiovicto546 <caiovicto@gmail.com>
2019-01-08 12:43:58 -0200

    Adicionar arquivos de projeto.

---
+++ ExercicioFixHerPoli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioFixHerPoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
